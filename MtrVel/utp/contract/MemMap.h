51f39bf7fa839a1e48bcfbabc6ceee93322efbd8
user <user@user.com>
2023-09-30 02:23:48 +0200

    Initial

---
+++ MtrVel/utp/contract/MemMap.h
@@ -0,0 +1,23 @@
+/*****************************************************************************
+* Copyright 2011 MyCompany Automotive, All Rights Reserved.
+* MyCompany Confidential
+*
+* Module File Name  : Compiler_Cfg.h
+* Module Description: This file contains a stub header for UTP and QAC 
+*                     projects
+* Product           : Gen II Plus EA3.0
+* Author            : Lucas Wendling
+*****************************************************************************/
+/*---------------------------------------------------------------------------
+* Version Control:
+* Date Created:      Fri May  9 16:48:22 2003
+* %version:          1 %
+* %derived_by:       rz3h1n %
+* %date_modified:    Tue Oct  9 09:50:38 2012 %
+*---------------------------------------------------------------------------*/
+#ifndef MEMMAP_H
+#define MEMMAP_H
+
+
+
+#endif  /* MEMMAP_H */
